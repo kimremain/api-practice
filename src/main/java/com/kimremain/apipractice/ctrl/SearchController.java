af04f88aee019d595991cf687615a5c09d1905da
kimremain <kimremain@gmail.com>
2019-01-06 13:07:19 +0900

    produces test

--- src/main/java/com/kimremain/apipractice/ctrl/SearchController.java
+++ src/main/java/com/kimremain/apipractice/ctrl/SearchController.java
@@ -1,5 +1,6 @@
 package com.kimremain.apipractice.ctrl;
 
+import com.google.gson.Gson;
 import com.kimremain.apipractice.service.SearchService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,26 +9,47 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 @AllArgsConstructor
 public class SearchController extends BaseController{
     @Autowired
     private SearchService searchService;
 
-
-    @GetMapping("/search")
-    public ModelAndView search(@RequestParam(value = "location", defaultValue = "서울") String location,
+    @GetMapping(value = "/search")
+    public ModelAndView searchHtml(@RequestParam(value = "location", defaultValue = "서울") String location,
                                @RequestParam(value = "keyword", defaultValue = "중식당") String keyword) throws Exception {
-        logger.debug("call SearchController main...");
-        searchService = new SearchService();
-        ModelAndView view = new ModelAndView("main");
+        logger.debug("call SearchController searchHtml...");
         logger.debug("location:"+location);
         logger.debug("keyword:"+keyword);
+
+        ModelAndView view = new ModelAndView("main");
+        searchService = new SearchService();
         view.addObject("location", location);
         view.addObject("keyword", keyword);
         view.addObject("posts", searchService.getData(location,keyword));
 
         //model.addAttribute("posts", searchService.getData("서울","중식당"));
         return view;
+    }
+
+    @GetMapping(value = "/search", produces="application/json")
+    public String searchJson(@RequestParam(value = "location", defaultValue = "서울") String location,
+                         @RequestParam(value = "keyword", defaultValue = "중식당") String keyword) throws Exception {
+        logger.debug("call SearchController searchJson...");
+        logger.debug("location:"+location);
+        logger.debug("keyword:"+keyword);
+
+        searchService = new SearchService();
+        Gson gson = new Gson();
+        String jsonArray = gson.toJson(searchService.getData(location,keyword));
+
+        return jsonArray;
+    }
+
+    @GetMapping(value = "/searchMerge")
+    public ModelAndView searchMerge(@RequestParam(value = "location", defaultValue = "서울") String location,
+                                   @RequestParam(value = "keyword", defaultValue = "중식당") String keyword) throws Exception {
+
+        return null;
     }
 }
